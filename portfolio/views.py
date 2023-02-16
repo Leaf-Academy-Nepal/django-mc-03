4cd1c64c493dbfb7323895c235ceb4d8f399632a
AarushThapa <aarushthapa2@gmail.com>
2023-02-16 21:49:01 +0545

    day 3

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import TemplateView
-
+from cv.models import Skill
 
 # yeha hamle django le diyeko kunai pani html file open garna lai or server garna lai TemplateView use gareko ho
 # IndexPageView -> yo function or class  call garne bitikai - index.html file open gar 
 
 class IndexPageView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        #select * from skill
+        data["skill_list"] = Skill.objects.all()
+        return data
